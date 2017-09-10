34648d8aca85ddb04a41b751ad991269829d6ef8
nathon <730382930@qq.com>
2017-09-11 01:36:52 +0800

    modify sql

--- src/main/resources/sql/ssm_crud.sql
+++ src/main/resources/sql/ssm_crud.sql
@@ -3,21 +3,21 @@ DROP DATABASE ssm_crud;
 CREATE DATABASE ssm_crud;
 
 # 创建员工数据表
-CREATE TABLE IF NOT EXISTS _emp (
-	emp_id INT(11) UNSIGNED AUTO_INCREMENT,
-	emp_name VARCHAR(255) NOT NULL,
-	gender CHAR(1),
-	email VARCHAR(255),
-	dept_id INT(11),
-	PRIMARY KEY (emp_id)
-) ENDINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `ssm_crud`.`_emp` (
+  `emp_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `emp_name` VARCHAR(255) NOT NULL,
+  `gender` CHAR(1) NULL,
+  `email` VARCHAR(255) NULL,
+  `d_id` INT(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`emp_id`)
+  ) ENDINE=InnoDB DEFAULT CHARSET=utf8;
 
 # 创建部门数据表
-CREATE TABLE IF NOT EXISTS _dept (
-	dept_id INT(11) UNSIGNED AUTO_INCREMENT,
-	dept_name VARCHAR(255) NOT NULL,
-	PRIMARY KEY (dept_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `ssm_crud`.`_dept` (
+  `dept_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `dept_name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`dept_id`)
+  ) ENDINE=InnoDB DEFAULT CHARSET=utf8;
 
 # 关联外键
 ALTER TABLE `ssm_curd`.`_emp`
