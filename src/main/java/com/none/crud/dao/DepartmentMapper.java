6d122caba47704d19ff0abc21dc82edf8b179a4e
nathon <730382930@qq.com>
2017-09-11 01:45:55 +0800

    mybatis generator

---
+++ src/main/java/com/none/crud/dao/DepartmentMapper.java
@@ -0,0 +1,30 @@
+package com.none.crud.dao;
+
+import com.none.crud.bean.Department;
+import com.none.crud.bean.DepartmentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DepartmentMapper {
+    long countByExample(DepartmentExample example);
+
+    int deleteByExample(DepartmentExample example);
+
+    int deleteByPrimaryKey(Integer deptId);
+
+    int insert(Department record);
+
+    int insertSelective(Department record);
+
+    List<Department> selectByExample(DepartmentExample example);
+
+    Department selectByPrimaryKey(Integer deptId);
+
+    int updateByExampleSelective(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByExample(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByPrimaryKeySelective(Department record);
+
+    int updateByPrimaryKey(Department record);
+}
